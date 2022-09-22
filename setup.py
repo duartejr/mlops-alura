7ed28e7b618d80525de5524d67afcb8fe70c7527
duartejr <duarte.jr105@gmail.com>
2022-09-22 10:43:58 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Desenvolvimento de um projeto de deploy de um modelo de Machine Learnning desenvolvido no curso de MLOps da Alura.',
+    author='Duarte Junior',
+    license='MIT',
+)
